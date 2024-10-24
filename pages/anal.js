d69e3a881ac900d5384215e5c883b13ba3f3146f
3vomanx <giorgigogidze39@gmail.com>
2024-10-24 22:10:28 +0400

    .

--- pages/anal.js
+++ pages/anal.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-
+import Head from "next/head";
 const Anal = ({isContentWide}) => {
   return (
     <div>
-      <Helmet>
+      <Head>
         <title>Adult live Anal cam | Wettify</title>
         <meta property="title" content="Adult live Anal cam | Wettify" />
         <meta property="og:title" content="Adult live Anal cam | Wettify" />
@@ -16,7 +16,7 @@ const Anal = ({isContentWide}) => {
           name="description"
           content="Discover the hottest live cam girls Anal! Enjoy private chats or camgirls stripchat live shows with beautiful models ready to entertain you"
         ></meta>
-      </Helmet>
+      </Head>
        <div className={`CategoryContent ${isContentWide ? 'Wide' : ''}`} >
        <iframe src="https://chaturbate.com/in/?tour=x1Rd&campaign=ccOiu&track=default&c=200&p=1&gender=f&tag=anal" className="rubateFeedIframe"></iframe>
         <p className="PageTextContent">
