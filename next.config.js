f95ec1ec3ab88f45e96953886eec23f4f5eee652
3vomanx <giorgigogidze39@gmail.com>
2024-11-07 23:20:10 +0400

    .

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-src 'self' https://chaturbate.com; child-src 'self' https://chaturbate.com;",
-          },
-        ],
-      },
-    ];
-  },
-};
-
+    output: 'export'
+  };
+  
 module.exports = nextConfig;
